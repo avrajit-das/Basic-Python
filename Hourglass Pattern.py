d3c536c4ffe4d9d3268d5e815d66c6256d066eb3
Avrajit Das <dasavrajit7@gmail.com>
2023-01-08 19:31:47 +0530

    Hourglass Pattern

---
+++ Hourglass Pattern.py
@@ -0,0 +1,20 @@
+row = int(input("Enter the number of rows: "))
+
+k= rows-2
+
+for i in range(rows,-1,-1):
+	for j in range(k,0,-10):
+		print(end = " ")
+
+		k= k+1
+
+		for j in range(0,i+1):
+			print("*",  end = " ")
+			print()
+
+			k= 2*rows-2
+
+			for i in range(0, rows + 1):
+				for j in range(0,k):
+					print(end = " ")
+					print()
